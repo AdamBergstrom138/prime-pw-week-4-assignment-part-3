e5f5cd39eaf74a5e3f76a1ff43d04160cc906f80
Adam Bergstrom <abx1138@gmail.com>
2022-11-19 16:17:31 -0600

    completed all questions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,6 +3,7 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 // Create a global variable named `basket` and set it to an empty array.
+// Add a global `const` named `maxItems` and set it to 5.
 let basket = [];
 const maxItems = 5;
 
@@ -33,18 +34,21 @@ function addItem(item) {
     if (isFull()){
         console.log('running addItem, basket is too full for:', item);
         return false;
+        // Not adding anything to the basket if the basket is full
     } else {
         console.log('running addItem:', item);
         basket.push(item);
         return true;
+        // Adding item to the end of basket array if the basket is not full
     }
 }
-//Test - Adding soda, cake, pizza, chips to basket.
+//Test - Adding soda, cake, pizza, chips, dip to basket.
 console.log('Test - adding soda to basket.', addItem('soda'));
 console.log('Test - adding cake to basket.', addItem('cake'));
 console.log('Test - adding pizza to basket.', addItem('pizza'));
 console.log('Test - adding chips to basket.', addItem('chips'));
 console.log('Test - adding dip to basket.', addItem('dip'));
+//Test - fish should not be added as the basket now has maxItems
 console.log('Test - adding fish to basket.', addItem('fish'));
 console.log('Test - Basket should have 5 items.', basket);
 /*
@@ -56,11 +60,40 @@ function listItems() {
     console.log('running listItems:')
     for (let i=0; i<basket.length; i++){
         console.log(basket[i]);
+        // console.logs each item in the basket array
     }
     return true;
 }
 //Test - All items in the basket should each be listed on a new line.
 console.log('Test - listItems:', listItems());
+
+/*
+Create a function called `removeItem`. It should:
+  - Take an input parameter for a string `item`
+  - Use [Array.indexOf] to find the index of the first matching item in the basket.
+  - Use [Array.splice] to remove the first matching item from the basket.
+  - Return the item removed or `null` if the item was not found
+*/
+function removeItem(item){
+    console.log('running removeItem');
+    if (basket.indexOf(item) >= 0){  // this will look for the item in the basket array if it doesn't exist it'll return -1
+        let removedLoc = basket.indexOf(item);
+        console.log('removedLoc:', removedLoc);
+        // Adding the above 2 lines to find the location of the item in the array.
+        basket.splice(removedLoc, 1); // removing the one item
+        return item;
+    } else {
+        return null;
+    }
+}
+//Test - Testing removeItem, removing cake. Should return cake.
+console.log('Test - removeItem cake:', removeItem('cake'));
+listItems(); // cake should be gone.
+basket;
+//Test - Testing removeItem, removing candy.  Should return null.
+console.log('Test - removeItem candy:', removeItem('candy'));
+listItems(); // no change.
+
 /*
 - Create a function called `empty`. It should:
   - reset the `basket` to an empty array
@@ -68,6 +101,7 @@ console.log('Test - listItems:', listItems());
 function empty() {
     console.log('running empty');
     basket = [];
+    // setting basket back to an empty array
     return true;
 }
 //Test - empty should return true and the basket should be empty.
